c77c1c9f1920073ce6f0b05e539c869c1a71c691
Sergey Kornyushin <taurus.sed9@gmail.com>
2021-11-01 21:51:15 +0700

    Change reverseText method to reverseString and reverseWord. Replacing all ArrayList to char[]. Add lambda to the onClick method.

--- app/src/main/java/com/example/task1anagrams/ReverseText.java
+++ app/src/main/java/com/example/task1anagrams/ReverseText.java
@@ -1,99 +1,63 @@
 package com.example.task1anagrams;
 
-import android.text.TextUtils;
-import java.util.ArrayList;
-
 public class ReverseText {
-    public static String reverseText(String myString, char[] inputTextToArray) {
 
-        ArrayList<Character> listOfExceptions = new ArrayList<Character>();
-        StringBuilder tempExceptionString = new StringBuilder();
+    public static String reverseString(String inputText, String inputExceptions){
+        StringBuilder finalString = new StringBuilder();
 
-        for (char c : inputTextToArray) {
-            if (c != ' ') {
-                tempExceptionString.append(c);
-            }
-        }
-        char[] exceptions = tempExceptionString.toString().toCharArray();
-
-        for (char exception : exceptions) {
-            if (!listOfExceptions.contains(exception)) {
-                listOfExceptions.add(exception);
-            }
-        }
-        String exceptionsString = TextUtils.join("", listOfExceptions);
-
-
-        ArrayList<Character> listFinal = new ArrayList<>();
-
-        char[] allExceptionsArray = exceptionsString.toCharArray();
-        ArrayList<Character> listExceptions = new ArrayList<>();
-
-        for (char exceptionSymbol : allExceptionsArray) {
-            listExceptions.add(exceptionSymbol);
-        }
-
-        String[] splitWords = myString.split(" ");
-
-        for (String word : splitWords) {
-
-            char[] wordArray = word.toCharArray();
-            ArrayList<Character> listInput = new ArrayList<>();
-
-            for (char inputSymbol : wordArray) {
-                listInput.add(inputSymbol);
-            }
-
-            int exceptionIndex = 0;
-            int startIndex = 0;
-            int lastIndex = listInput.size() - 1;
-            outer:
-            while (startIndex < lastIndex) {
-                char tempFirst = listInput.get(startIndex);
-                inner:
-                while (lastIndex > startIndex) {
-                    char tempLast = listInput.get(lastIndex);
-                    if (listExceptions.size() == 0) {
-                        listInput.remove(startIndex);
-                        listInput.add(startIndex, tempLast);
-                        listInput.remove(lastIndex);
-                        listInput.add(lastIndex, tempFirst);
-                        startIndex++;
-                        lastIndex--;
-                        continue outer;
-                    }
-                    while (exceptionIndex < listExceptions.size()) {
-                        if (listExceptions.get(exceptionIndex) == tempFirst) {
-                            startIndex++;
-                            exceptionIndex = 0;
-                            continue outer;
-                        } else if (listExceptions.get(exceptionIndex) == tempLast) {
-                            lastIndex--;
-                            exceptionIndex = 0;
-                            continue inner;
-                        } else {
-                            if (exceptionIndex != listExceptions.size() - 1) {
-                                exceptionIndex++;
-                            } else {
-                                listInput.remove(startIndex);
-                                listInput.add(startIndex, tempLast);
-                                listInput.remove(lastIndex);
-                                listInput.add(lastIndex, tempFirst);
-                                startIndex++;
-                                lastIndex--;
-                                exceptionIndex = 0;
-                                continue outer;
-                            }
-                        }
-                    }
-                    lastIndex--;
-                }
-                startIndex++;
-            }
-            listFinal.addAll(listInput);
-            listFinal.add(' ');
-        }
-        return TextUtils.join("", listFinal);
+        String[] splitWords = inputText.split(" ");
+        for (String word : splitWords){
+            finalString.append(reverseWord(word, inputExceptions)).append(" ");
+        } return finalString.toString();
     }
 
+    public static String reverseWord(String word, String exceptions) {
+
+        char[] wordArray = word.toCharArray();
+        char[] exceptionsArray = exceptions.toCharArray();
+
+        int exceptionIndex = 0;
+        int startIndex = 0;
+        int lastIndex = wordArray.length - 1;
+        outer:
+        while (startIndex < lastIndex) {
+            char tempFirst = wordArray[startIndex];
+            inner:
+            while (lastIndex > startIndex) {
+                char tempLast = wordArray[lastIndex];
+                if (exceptionsArray.length == 0) {
+                    wordArray[startIndex] = tempLast;
+                    wordArray[lastIndex] = tempFirst;
+                    startIndex++;
+                    lastIndex--;
+                    continue outer;
+                }
+                while (exceptionIndex < exceptionsArray.length) {
+                    if (exceptionsArray[exceptionIndex] == tempFirst) {
+                        startIndex++;
+                        exceptionIndex = 0;
+                        continue outer;
+                    } else if (exceptionsArray[exceptionIndex] == tempLast) {
+                        lastIndex--;
+                        exceptionIndex = 0;
+                        continue inner;
+                    } else {
+                        if (exceptionIndex != exceptionsArray.length - 1) {
+                            exceptionIndex++;
+                        } else {
+                            wordArray[startIndex] = tempLast;
+                            wordArray[lastIndex] = tempFirst;
+                            startIndex++;
+                            lastIndex--;
+                            exceptionIndex = 0;
+                            continue outer;
+                        }
+                    }
+                }
+                lastIndex--;
+            }
+            startIndex++;
+        }
+        return String.valueOf(wordArray);
+    }
 }
