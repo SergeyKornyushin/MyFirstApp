1f9b60d26fb848fdc1e6b452285293afaa424411
Sergey Kornyushin <taurus.sed9@gmail.com>
2021-11-01 23:04:00 +0700

    Add string values.
    Change reverseText method to reverseString and reverseWord.
    Replacing all ArrayList to char[].
    Add lambda to the onClick method.

---
+++ app/src/main/java/com/example/task1anagrams/ReverseText.java
@@ -0,0 +1,64 @@
+package com.example.task1anagrams;
+
+public class ReverseText {
+
+    public static String reverseString(String inputText, String inputExceptions){
+        StringBuilder finalString = new StringBuilder();
+
+        String[] splitWords = inputText.split(" ");
+        for (String word : splitWords){
+            finalString.append(reverseWord(word, inputExceptions)).append(" ");
+        } return finalString.toString();
+    }
+
+    public static String reverseWord(String word, String exceptions) {
+
+        char[] wordArray = word.toCharArray();
+        char[] exceptionsArray = exceptions.toCharArray();
+        String reversedWord;
+
+        int exceptionIndex = 0;
+        int startIndex = 0;
+        int lastIndex = wordArray.length - 1;
+        outer:
+        while (startIndex < lastIndex) {
+            char tempFirst = wordArray[startIndex];
+            inner:
+            while (lastIndex > startIndex) {
+                char tempLast = wordArray[lastIndex];
+                if (exceptionsArray.length == 0) {
+                    wordArray[startIndex] = tempLast;
+                    wordArray[lastIndex] = tempFirst;
+                    startIndex++;
+                    lastIndex--;
+                    continue outer;
+                }
+                while (exceptionIndex < exceptionsArray.length) {
+                    if (exceptionsArray[exceptionIndex] == tempFirst) {
+                        startIndex++;
+                        exceptionIndex = 0;
+                        continue outer;
+                    } else if (exceptionsArray[exceptionIndex] == tempLast) {
+                        lastIndex--;
+                        exceptionIndex = 0;
+                        continue inner;
+                    } else {
+                        if (exceptionIndex != exceptionsArray.length - 1) {
+                            exceptionIndex++;
+                        } else {
+                            wordArray[startIndex] = tempLast;
+                            wordArray[lastIndex] = tempFirst;
+                            startIndex++;
+                            lastIndex--;
+                            exceptionIndex = 0;
+                            continue outer;
+                        }
+                    }
+                }
+                lastIndex--;
+            }
+            startIndex++;
+        }
+        return reversedWord = String.valueOf(wordArray);
+    }
+}
