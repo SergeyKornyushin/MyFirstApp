db3cf4d667445bac0bb949190131fac8e43f5180
Sergey Kornyushin <taurus.sed9@gmail.com>
2022-05-09 03:59:24 +0700

    Add jUnit and Espresso tests

--- app/src/androidTest/java/com/example/task1anagrams/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/task1anagrams/ExampleInstrumentedTest.java
@@ -1,26 +1,40 @@
 package com.example.task1anagrams;
 
-import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
+import androidx.test.rule.ActivityTestRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.Rule;
 
-import static org.junit.Assert.*;
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.task1anagrams", appContext.getPackageName());
+
+    public void clickButtonReverse() {
+        String[] inputText = {"Foxminded", "Foxminded cool 24/7", "a1bcd efg!h"};
+        String[] exceptions = {"x", "xl", ""};
+        String[] results = {"dexdnimoF", "dexdnimoF oocl 7/42", "d1cba hgf!e"};
+        for (int i = 0; i < inputText.length; i++) {
+            onView(withId(R.id.edt_exception_symbols)).perform(clearText());
+            onView(withId(R.id.edt_input_text)).perform(clearText());
+            onView(withId(R.id.edt_exception_symbols)).perform(typeText(exceptions[i]), closeSoftKeyboard());
+            onView(withId(R.id.edt_input_text)).perform(typeText(inputText[i]), closeSoftKeyboard());
+            onView(withId(R.id.btn_convert)).perform(click());
+            onView(withId(R.id.txt_reverse)).check(matches(withText(results[i])));
+        }
     }
+
 }
+
+
